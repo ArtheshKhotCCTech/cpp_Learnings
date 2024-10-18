c10f4b180e6aa233b6f390ea2d126c72c46969ae
ArtheshKhot <145855416+ArtheshKhot@users.noreply.github.com>
2024-10-18 06:35:16 +0530

    Commit

---
+++ ConstantsandInline/HeaderFiles/MyConstant.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class MyConstant {
+public:
+    static const int MAX_LENGTH;
+    static const int MAX_WIDTH;
+    static const int MAX_SIDE;
+    static const int MAX_RADIUS;
+    static const double PI;
+};
---
+++ ConstantsandInline/src/MyConstant.cpp
@@ -0,0 +1,7 @@
+#include "../HeaderFiles/MyConstant.h"
+
+const int MyConstant::MAX_LENGTH = 100;
+const int MyConstant::MAX_WIDTH = 50;
+const int MyConstant::MAX_SIDE = 50;
+const int MyConstant::MAX_RADIUS = 30;
+const double MyConstant::PI = 3.14159;
